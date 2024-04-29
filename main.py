21f121ecd7f8d8ed1fc95c4e860ed52730dc6ffe
username <you@example.com>
2024-04-29 16:47:30 -0500

    updated main.py to open and print text file and return word count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def main():
+    #opens a file from the books directory and saves the text as file_contents string
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
@@ -6,11 +7,14 @@ def main():
 
     print(file_contents)
 
-    word_count(file_contents)
+    count = word_count(file_contents)
 
+    print(count)
+
+#takes a string and returns the word count
 def word_count(book_text):
     count = len(book_text.split())
-    print(count)
+    return count
 
 
 
